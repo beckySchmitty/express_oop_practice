13d6dc22b29919351ed861c0cc5fd57c566b7874
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-27 19:42:14 -0500

    end of notes on first approach

--- models/cat.js
+++ models/cat.js
@@ -13,6 +13,47 @@ class Cat {
       }
       return result.rows[0];
   }
+  static async create(name, age) {
+    if (!name || !age) {
+      throw new ExpressError("Missing required data. Must have name and age", 400)
+    }
+    const result = await db.query(`INSERT INTO cats
+     (name, age) VALUES ($1, $2) 
+     RETURNING id, name, age`, [name, age]);
+    return result.rows[0];
+}
+
+static async delete(id) {
+  const result = await db.query(`DELETE FROM cats WHERE id=$1 RETURNING id`, [id])
+  if (result.rows.length === 0) {
+    throw new ExpressError("Cat not found", 404)
+  }
+}
+
+static async update(id, newName, newAge) {
+  const result = await db.query(`
+    UPDATE cats SET name = $1, age  = $2 
+    WHERE id = $3
+    RETURNING id, name, age
+  `, [newName, newAge, id])
+  if (result.rows.length === 0) {
+    throw new ExpressError("Cat not found", 404)
+  }
+  return result.rows[0]
+}
+
+static async makeOlder(id) {
+  const result = await db.query(`
+    UPDATE cats SET age  = age + 1
+    WHERE id = $1
+    RETURNING id, name, age
+  `, [id])
+  if (result.rows.length === 0) {
+    throw new ExpressError("Cat not found", 404)
+  }
+  return result.rows[0]
+}
+
 }
 module.exports = Cat;
 
--- routes/cats.js
+++ routes/cats.js
@@ -25,6 +25,45 @@ router.get('/:id', async (req, res, next) => {
   }
 })
 
+router.post('/', async (req, res, next) => {
+  try {
+    const {name, age} = req.body;
+    const cat = await Cat.create(name, age)
+    return res.json(cat);
+  } catch (e) {
+    return next(e)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await Cat.delete(req.params.id);
+    return res.json({msg: "deleted"});
+  } catch (e) {
+    return next(e)
+  }
+})
+
+
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { name, age } = req.body;
+    const cat = await Cat.update(req.params.id, name, age)
+    return res.json(cat)
+  } catch (e) {
+    return next(e)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    const cat = await Cat.makeOlder(req.params.id);
+    return res.json(cat)
+  } catch (e) {
+    return next(e)
+  }
+})
+
 
 module.exports = router;
 
