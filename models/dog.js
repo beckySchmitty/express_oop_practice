9475bcaec2e58eee741c977d64a9499cc75dad34
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-01-27 21:41:56 -0500

    first dog routes - mini ORM notes

---
+++ models/dog.js
@@ -0,0 +1,40 @@
+// "smarter OO model" - like mini ORM
+// similar to SQLAlchemy
+// will instantiate model, then can call methods on the instances
+// each will hold specific data to one dog
+// will still have static methods
+
+const db = require("../db");
+const ExpressError = require("../expressError")
+
+class Dog {
+    constructor(id, name, age) {
+        this.is = id;
+        this.name = name;
+        this.age = age;
+    }
+    static async getAll() {
+       const results = await db.query(`SELECT id, name, age FROM dogs`);
+       const dogs = results.rows.map( r => new Dog(r.id, r.name, r.age));
+       return dogs;
+    }
+
+    static async getById(id) {
+        const results = await db.query(`SELECT id, name, age
+        FROM dogs
+        WHERE id=$1`, [id])
+        const d = results.rows[0]
+        if (!d) {
+            throw new ExpressError("Dog not found", 404)
+        }
+        return new Dog(d.id, d.name, d.age)
+    }
+
+    speak() {
+        console.log(`${this.dog} says WOOOOF`)
+    }
+
+
+}
+
+module.exports = Dog;
